3eda819ca7feb82814cbb544d63847c26f5d4eb3
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2022-09-09 10:22:53 +0200

    Add logging

--- gpio.ts
+++ gpio.ts
@@ -1,11 +1,16 @@
 import {Gpio} from 'onoff'
 
+console.log('creating Gpio instances')
 const garden = new Gpio(24, 'high')
 const pump = new Gpio(23, 'high')
+console.log('created.')
 
 const run = async () => {
 	try {
-		await garden.write(Gpio.LOW)
+		console.log('setting gerden to low')
+		await garden.activeLow()
+		console.log('set')
+
 	} catch (e) {
 		console.error('error', e)
 	}
