4c74f2c388f771d2d5475acdb0720071b03236f8
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2022-09-17 09:04:25 +0200

    Add switches endpoint

--- pages/api/swithces.ts
+++ pages/api/swithces.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import {getState, gpioState} from "../../gpio";
 
 const switches = (req: NextApiRequest, res: NextApiResponse) => {
+	console.log('switches method', req.method)
 	if (req.method === 'GET') {
 		console.log('get switches')
 		const state = getState()
