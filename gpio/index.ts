a9f7251528f96e1fbbe06041c78b7d6287aab1ce
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2022-09-17 09:10:09 +0200

    More readable state

--- gpio/index.ts
+++ gpio/index.ts
@@ -4,8 +4,15 @@ import inventory from "../inventory";
 type State = {
 	[name: string]: Gpio
 }
-
 const gpioState: State = {}
+
+type ReadableState = {
+	[name: string]: {
+		name: string,
+		state: 'on' | 'off',
+		gpio: number
+	}
+}
 // Start with all inventory states to be false.
 Object.keys(inventory).forEach(iKey => {
 	// Relay controller is ON on LOW. Initializing with HIGH so it starts as OFF.
@@ -13,11 +20,21 @@ Object.keys(inventory).forEach(iKey => {
 	console.log('setting up', iKey, inventory[iKey])
 })
 
-const getState = () => Object.keys(inventory).map(iKey => {
-	console.log('get state for ', iKey)
-	const keyState = gpioState[iKey].readSync()
-	console.log('keyState', keyState)
-	return Boolean(keyState)
-})
+const getState = () => {
+	const result: ReadableState = {}
+	Object.keys(inventory).forEach(iKey => {
+		console.log('get state for ', iKey)
+		const keyState = gpioState[iKey].readSync()
+		console.log('keyState', keyState)
+		result[iKey] = {
+			name: iKey,
+			// Switch board is low-based
+			state: gpioState[iKey].readSync() === Gpio.HIGH ? 'off' : 'on',
+			gpio: inventory[iKey]
+		}
 
+	})
+	return result
+
+}
 export {gpioState, getState}
